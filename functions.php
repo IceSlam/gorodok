5826489d9c82ca55b44ec7a205e7c94bdacd53fb
IceSlam <mainguy@mail.ru>
2021-02-18 01:43:03 +0300

    Renamed WordPress Bootstrap NavWalker file to class-bnavwalker.php

--- functions.php
+++ functions.php
@@ -256,12 +256,12 @@ add_filter('acf/options_page/settings', 'my_acf_options_page_settings');
 =================================================================================
 */
 
-if ( ! file_exists( get_template_directory() . '/class-wp-bootstrap-navwalker.php' ) ) {
+if ( ! file_exists( get_template_directory() . '/class-bnavwalker.php' ) ) {
 // File does not exist... return an error.
-    return new WP_Error( 'class-wp-bootstrap-navwalker-missing', __( 'It appears the class-wp-bootstrap-navwalker.php file may be missing.', 'wp-bootstrap-navwalker' ) );
+    return new WP_Error( 'class-wp-bootstrap-navwalker-missing', __( 'It appears the class-bnavwalker.php file may be missing.', 'wp-bootstrap-navwalker' ) );
 } else {
     // File exists... require it.
-    require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+    require_once get_template_directory() . '/class-bnavwalker.php';
 }
 
 
